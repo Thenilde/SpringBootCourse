bf0b5e2d3eef8e7b001ff3a64ffbc47e3314e50d
Thenilde Borges <thenilde.tb@hotmail.com>
2023-07-16 23:07:16 +0100

    Order, Instant, ISO 8601

--- src/main/java/com/borgesdev/course/entities/User.java
+++ src/main/java/com/borgesdev/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.borgesdev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -21,7 +24,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order>orders=new ArrayList<>();
+	
 	public User() {
 
 	}
@@ -71,6 +77,9 @@ public class User implements Serializable{
 		return password;
 	}
 
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -91,6 +100,8 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
