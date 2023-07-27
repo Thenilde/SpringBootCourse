5770b4d38f3ae12e261015e98285776e1de5aa5e
Thenilde Borges <thenilde.tb@hotmail.com>
2023-07-27 01:29:54 +0100

    User Inssert

--- src/main/java/com/borgesdev/course/servicies/UserService.java
+++ src/main/java/com/borgesdev/course/servicies/UserService.java
@@ -11,18 +11,23 @@ import com.borgesdev.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	//implemente de 2 operacoes basicas :1 pra buscar todos os users and a
-	// 2 outra pra buscar um user por id.  
-	
-	@Autowired //dependecy
+	// implemente de 2 operacoes basicas :1 pra buscar todos os users and a
+	// 2 outra pra buscar um user por id.
+
+	@Autowired // dependecy
 	private UserRepository repository;
-	
-	public List<User>findAll(){//metodo para retornar todos os users do banco de dado.
+
+	public List<User> findAll() {// metodo para retornar todos os users do banco de dado.
 		return repository.findAll();
-	
-}
+
+	}
+
 	public User findById(Long id) {
-		Optional<User>obj=repository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
